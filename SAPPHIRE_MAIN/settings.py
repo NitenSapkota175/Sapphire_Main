36a5a7e42c6c3f65913e5b46ac3b83b8f3295cab
NitenSapkota175 <nitensapkota1705@gmail.com>
2022-06-22 10:09:37 +0530

    domain adeed

--- SAPPHIRE_MAIN/settings.py
+++ SAPPHIRE_MAIN/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-7hljt!*b3j*)g%o5zt+ql!3mg$q$xk(ocd=_ex5a1atx-!$)@+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['www.sapphirecommotrade.com','sapphirecommotrade.com','localhost']
 
 
 # Application definition
