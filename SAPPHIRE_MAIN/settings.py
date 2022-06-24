1da0a1ecbc6e2dd93afc3b5400c183a5014e498b
NitenSapkota175 <nitensapkota1705@gmail.com>
2022-06-24 19:13:05 +0530

    recreate some files

--- SAPPHIRE_MAIN/settings.py
+++ SAPPHIRE_MAIN/settings.py
@@ -135,10 +135,10 @@ MEDIA_URL = '/images/'
 
 
 STATICFILES_DIRS =  [
-        os.path.join(BASE_DIR , 'static_in_env')
+        os.path.join(BASE_DIR , 'Static')
     ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-MEDIA_ROOT = os.path.join(BASE_DIR , 'static/images')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_cdn')
+MEDIA_ROOT = os.path.join(BASE_DIR , 'media_cdn')
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
