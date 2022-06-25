7e78d55d642a78523219a7f3489fe3e646ee056a
NitenSapkota175 <nitensapkota1705@gmail.com>
2022-06-25 17:41:59 +0530

    added som conf

--- Sapphire/urls.py
+++ Sapphire/urls.py
@@ -2,12 +2,12 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('',views.HomePage,name='Home'),
-    path('Aboutus/',views.AboutUsPage,name='Aboutus'),
+    path('AboutUs/',views.AboutUsPage,name='Aboutus'),
     path('products/',views.ProductPage,name='Product'),
     path('product_type1/<str:pk>',views.ProductType1,name='ProductType1'),
     path('product_type2/<str:pk>',views.ProductType2,name='ProductType2'),
     path('ourwork',views.OurWork,name='Ourwork'),
-    path('contactus/',views.Contactus,name='contactus'),
+    path('ContactUs/',views.Contactus,name='contactus'),
    
 ]
 
