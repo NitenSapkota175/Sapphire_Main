5c2911919db2aa0c55739bd88978dab770c257d0
NitenSapkota175 <nitensapkota1705@gmail.com>
2022-06-26 07:54:14 +0530

    added css

--- SAPPHIRE_MAIN/settings.py
+++ SAPPHIRE_MAIN/settings.py
@@ -133,10 +133,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'static'),
-]
-
 AWS_ACCESS_KEY_ID = "3RWENTXA6CAUUYKFTV7O"
 AWS_SECRET_ACCESS_KEY = "aVwwmmEcR2XUccrORbzCD/GsR8hQoEhEhtsWJre/Rg4" 
 AWS_STORAGE_BUCKET_NAME = "open-sapphiremain-spaces"
@@ -190,11 +186,11 @@ JAZZMIN_SETTINGS = {
     # Title on the brand (19 chars max) (defaults to current_admin_site.site_header if absent or None)
     "site_brand": "Sapphire",
 
-    "site_logo": "img/SAPPHIRE_COMMOTRADE_FINAL_Logo2.png",
+    "site_logo": "SAPPHIRE_COMMOTRADE_FINAL_Logo2.png",
 
 
         # Logo to use for your site, must be present in static files, used for login form logo (defaults to site_logo)
-    "login_logo": "img/SAPPHIRE_COMMOTRADE_FINAL_Logo2.png" ,
+    "login_logo": "SAPPHIRE_COMMOTRADE_FINAL_Logo2.png" ,
 
     # Logo to use for login form in dark themes (defaults to login_logo)
     "login_logo_dark": None,
@@ -205,7 +201,7 @@ JAZZMIN_SETTINGS = {
     "site_logo_dark" : None,
 
     # Relative path to a favicon for your site, will default to site_logo if absent (ideally 32x32 px)
-    "site_icon":"img/SAPPHIRE_COMMOTRADE_FINAL_Logo2.png" ,
+    "site_icon":"SAPPHIRE_COMMOTRADE_FINAL_Logo2.png" ,
 
     # Welcome text on the login screen
     "welcome_sign": "Welcome to the Sapphire Admin",
