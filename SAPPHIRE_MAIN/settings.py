8552cf59a7b68954482b60dfaa3b8990bd8d0e40
NitenSapkota175 <nitensapkota1705@gmail.com>
2022-06-26 08:38:28 +0530

    added static_dir without error

--- SAPPHIRE_MAIN/settings.py
+++ SAPPHIRE_MAIN/settings.py
@@ -133,7 +133,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATICFILES_DIRS = [
-    os.join.path('static'),
+   os.path.join(BASE_DIR,'static')
 ]
 
 AWS_ACCESS_KEY_ID = "3RWENTXA6CAUUYKFTV7O"
