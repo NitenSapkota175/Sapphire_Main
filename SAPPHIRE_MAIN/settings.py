f79c211328b44b0555458838f28c784373cbac73
NitenSapkota175 <nitensapkota1705@gmail.com>
2022-06-28 02:26:55 +0530

    changed smtp port

--- SAPPHIRE_MAIN/settings.py
+++ SAPPHIRE_MAIN/settings.py
@@ -165,10 +165,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Email settings 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
+EMAIL_PORT = 465
 EMAIL_HOST_USER = 'sapphireupvcwindows@gmail.com'
 EMAIL_HOST_PASWORD = 'hxtb iphp aifi ibob'
-EMAIL_USE_TLS = True
+EMAIL_USE_SSL = True
 #EMAIL_USE_TLS = FALSE
 
 
