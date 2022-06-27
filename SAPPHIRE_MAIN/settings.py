923123ddb250e8243c0b4ab0e2db3c15c3df30ca
NitenSapkota175 <nitensapkota1705@gmail.com>
2022-06-28 03:52:18 +0530

    s

--- SAPPHIRE_MAIN/settings.py
+++ SAPPHIRE_MAIN/settings.py
@@ -168,7 +168,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'pointers200@gmail.com'
 EMAIL_HOST_PASWORD = 'nitensapkota10101111'
-EMAIL_USE_TLS = True
+EMAIL_USE_TLS = FALSE
 #EMAIL_USE_TLS = FALSE
 
 
