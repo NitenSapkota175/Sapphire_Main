3e16dfae264605285211db9d1d1da4be5d7053b5
NitenSapkota175 <nitensapkota1705@gmail.com>
2022-07-25 16:57:48 +0530

    added some config

--- SAPPHIRE_MAIN/urls.py
+++ SAPPHIRE_MAIN/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 urlpatterns = [
 
     
-    #path('admin/',include('admin_honeypot.urls',namespace='admin_honey')),
+    path('admin/',include('admin_honeypot.urls',namespace='admin_honey')),
     path('sapphire_main_secret_10101111/', admin.site.urls),
     path('',include('Sapphire.urls')),
 ]
