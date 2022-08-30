bf2548c92fc9ec2d7b5458bcbfd37104d20c5040
Raman Tamang <ramantamang392@gmail.com>
2022-08-30 21:56:44 +0530

    brochureDiv added

--- SAPPHIRE_MAIN/settings.py
+++ SAPPHIRE_MAIN/settings.py
@@ -16,7 +16,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7hljt!*b3j*)g%o5zt+ql!3mg$q$xk(ocd=_ex5a1atx-!$)@+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 if DEBUG == False:
     ALLOWED_HOSTS = ['www.sapphirecommotrade.com','sapphirecommotrade.com','localhost']
@@ -34,7 +34,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'phonenumber_field',
-    'storages',
+    #'storages',
     #'admin_honeypot',
     
     'Sapphire.apps.SapphireConfig',
